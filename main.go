3654356cf5ab8589bdae9032d59071b003aa1276
Matt Nunogawa <@amattn>
2016-04-07 11:29:10 -0700

    HTTP version of Hello World

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+const DEFAULT_LISTEN_ADDRESS = ":8080"
 
 func main() {
 	log.Println("gtls", Version(), "build", BuildNumber())
 
-	// we are not actually a web server yet.
-	log.Println("Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World, you came from: %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Println("Listening from:", DEFAULT_LISTEN_ADDRESS)
+	log.Fatal(http.ListenAndServe(DEFAULT_LISTEN_ADDRESS, nil))
 }
